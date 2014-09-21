997a9a149a483589ae50c7af5d8eed5a75c5c5c3
Ilja Bauer <i.bauer@cuescience.de>
2014-09-21 23:33:47 +0200

    init

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='cuescience-test',
+    version='0.1.0',
+    description='Custom test runner for all projects',
+    maintainer='cuescience',
+    maintainer_email='kontakt@cuescience.de',
+    license="MIT",
+    url='',
+    packages=['test', ],
+)
---
+++ testing/runner.py
@@ -0,0 +1,35 @@
+from django.db.models import AutoField
+from django.test.runner import DiscoverRunner
+from teamcity.unittestpy import TeamcityTestRunner
+
+
+class GlobalUniqueAutoField(object):
+    counter = 0
+
+    @classmethod
+    def get_id(cls):
+        id = cls.counter
+        cls.counter += 1
+        return id
+
+
+class CuescienceTestRunner(DiscoverRunner):
+    def run_suite(self, suite, **kwargs):
+        return TeamcityTestRunner().run(suite)
+
+    def setup_test_environment(self, **kwargs):
+        super(CuescienceTestRunner, self).setup_test_environment(**kwargs)
+        from django.db import models
+
+        ms = models.get_models(include_auto_created=True)
+        for model in ms:
+            pk_field = model._meta.pk
+            if isinstance(pk_field, AutoField):
+                pk_field.default = GlobalUniqueAutoField.get_id
+            else:
+                # TODO use the logger here
+                print "The model {0} uses a {1} pk field!".format(model, type(pk_field))
+
+    def teardown_test_environment(self):
+        GlobalUniqueAutoField.counter = 0
+        super(CuescienceTestRunner, self).teardown_test_environment()
